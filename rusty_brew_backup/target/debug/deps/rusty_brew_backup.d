cd18ce02844a3cb7637bbe4a87538707604f153f
Jason Ross <jason.ross841@gmail.com>
2023-04-14 18:56:22 -0500

    initial setup

---
+++ rusty_brew_backup/target/debug/deps/rusty_brew_backup.d
@@ -0,0 +1,5 @@
+C:\Users\jason\workspace\rusty_brew_backup\rusty_brew_backup\target\debug\deps\rusty_brew_backup.exe: src\main.rs
+
+C:\Users\jason\workspace\rusty_brew_backup\rusty_brew_backup\target\debug\deps\rusty_brew_backup.d: src\main.rs
+
+src\main.rs:
---
+++ rusty_brew_backup/target/debug/rusty_brew_backup.d
@@ -0,0 +1,1 @@
+C:\Users\jason\workspace\rusty_brew_backup\rusty_brew_backup\target\debug\rusty_brew_backup.exe: C:\Users\jason\workspace\rusty_brew_backup\rusty_brew_backup\src\main.rs
