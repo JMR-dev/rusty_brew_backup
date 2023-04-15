86ed623fdd62a71028601d37b2d0674c4ceef32a
Jason Ross <jason.ross841@gmail.com>
2023-04-14 23:17:29 -0500

    fix folder structure

---
+++ target/debug/deps/rusty_brew_backup-9bb2e61b372a390a.d
@@ -0,0 +1,5 @@
+c:\Users\jason\workspace\rusty_brew_backup\rusty_brew_backup\target\debug\deps\rusty_brew_backup-9bb2e61b372a390a.rmeta: src\main.rs
+
+c:\Users\jason\workspace\rusty_brew_backup\rusty_brew_backup\target\debug\deps\rusty_brew_backup-9bb2e61b372a390a.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/rusty_brew_backup-e20163e7e3c96736.d
@@ -0,0 +1,5 @@
+c:\Users\jason\workspace\rusty_brew_backup\rusty_brew_backup\target\debug\deps\rusty_brew_backup-e20163e7e3c96736.rmeta: src\main.rs
+
+c:\Users\jason\workspace\rusty_brew_backup\rusty_brew_backup\target\debug\deps\rusty_brew_backup-e20163e7e3c96736.d: src\main.rs
+
+src\main.rs:
